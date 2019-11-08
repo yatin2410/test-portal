671cd1b03b4eaac90f9c39a6b5d621225132a09d
Yatin Patel <yatinpatel.gt@gmail.com>
2019-11-09 02:37:01 +0530

    takequiz-2

--- client/src/components/take-quiz/TakeQuiz/Question.js
+++ client/src/components/take-quiz/TakeQuiz/Question.js
@@ -3,8 +3,8 @@ import "./css/takequiz.css";
 
 class Question extends Component {
   render() {
-    console.log(this.props.activequestion);
     const {question,o1,o2,o3,o4} = this.props.activequestion;
+    const {onAnsChange,Index,ans} = this.props;
     return (
       <div className="container mt-5 ml-3 que">
         <div className="mt-3">
@@ -16,12 +16,11 @@ class Question extends Component {
         <div className="mt-3">
           <label forhtml="option"><b>Options:</b></label>
           <br/>
-          <div className="ml-4 mt-2"> <input type="radio"/> <label className="ml-1" dangerouslySetInnerHTML={{__html:o1}}></label></div>
-          <div className="ml-4 mt-2"> <input type="radio"/> <label className="ml-1" dangerouslySetInnerHTML={{__html:o2}}></label></div>
-          <div className="ml-4 mt-2"> <input type="radio"/> <label className="ml-1" dangerouslySetInnerHTML={{__html:o3}}></label></div>
-          <div className="ml-4 mt-2"> <input type="radio"/> <label className="ml-1" dangerouslySetInnerHTML={{__html:o4}}></label></div>
+          <div className="ml-4 mt-2" onClick={()=>onAnsChange(Index,1)}> <input type="radio" name="ans" checked={ans===1} /> <label className="ml-1" dangerouslySetInnerHTML={{__html:o1}}></label></div>
+          <div className="ml-4 mt-2" onClick={()=>onAnsChange(Index,2)}> <input type="radio" name="ans" checked={ans===2} /> <label className="ml-1" dangerouslySetInnerHTML={{__html:o2}}></label></div>
+          <div className="ml-4 mt-2" onClick={()=>onAnsChange(Index,3)}> <input type="radio" name="ans" checked={ans===3} /> <label className="ml-1" dangerouslySetInnerHTML={{__html:o3}}></label></div>
+          <div className="ml-4 mt-2" onClick={()=>onAnsChange(Index,4)}> <input type="radio" name="ans" checked={ans===4} /> <label className="ml-1" dangerouslySetInnerHTML={{__html:o4}}></label></div>
         </div>
-        
       </div>
     );
   }
--- client/src/components/take-quiz/TakeQuiz/takequiz.js
+++ client/src/components/take-quiz/TakeQuiz/takequiz.js
@@ -5,17 +5,17 @@ import Question from "./Question";
 import "./css/takequiz.css";
 import { fetchUserQuizFull } from "../../../actions/fetchActions";
 import classNames from "classnames";
+import Timer from "react-compound-timer";
 
 function sideQuestion(props) {
-    console.log(props.aid,props.qid);
   return (
-    <div class={"question-item mt-3 "+classNames({'selected':props.qid===props.aid})} onClick={()=>props.onChange(props.index)}>
+    <div class={"question-item mt-3 " + classNames({ 'selected': props.qid === props.aid })} onClick={() => props.onChange(props.index)}>
       <div class="inline-block float-left">
         <div class="dark float-left padding-10 width-100">
           <div class="padding-top-5 padding-left-5 float-left padding-right-5">
-            {props.index+1}.{" "}
+            {props.index + 1}.{" "}
           </div>
-          <div class="question-statement padding-top-5" dangerouslySetInnerHTML={{__html:props.question}}>
+          <div class="question-statement padding-top-5" dangerouslySetInnerHTML={{ __html: props.question }}>
           </div>
         </div>
       </div>
@@ -23,46 +23,101 @@ function sideQuestion(props) {
   );
 }
 
+class Timmer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time : null
+    };
+  }
+  componentWillReceiveProps(nextProps){
+    if(nextProps.Time){
+      this.setState({time:nextProps.Time});
+    }
+  }
+  render() {
+    return (
+      <div className="time-widget align-center">{this.state.time?
+        <Timer 
+          initialTime={this.state.time*60*1000}
+          direction="backward"
+          checkpoints={[
+            {
+              time:1000*60*5,
+              callback:()=>alert("only five minutes remained"),
+            },
+            {
+              time:0,
+              callback:()=>this.props.onSubmit()
+            }
+          ]}
+        >
+          Timer - <Timer.Minutes />:<Timer.Seconds/>
+        </Timer>
+        :
+        <div>Timer - 0:0</div>}
+      </div>
+    )
+  }
+}
+
+
 class TakeQuiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
       quiz: {},
-      activeIndex: 0
+      activeIndex: 0,
+      anss: [],
     };
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onNext = this.onNext.bind(this);
     this.onPrev = this.onPrev.bind(this);
+    this.onClearResponse = this.onClearResponse.bind(this);
+    this.onAnsChange = this.onAnsChange.bind(this);
   }
   componentDidMount() {
     this.props.fetchUserQuizFull(this.props.match.params.id);
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.quiz) {
-      console.log(nextProps.quiz.quiz);
+      let arr = [];
+      for (let i = 0; i < parseInt(nextProps.quiz.quiz.questionsFull.length); i++) {
+        arr.push(-1);
+      }
+      this.setState({ anss: arr });
       this.setState({ quiz: nextProps.quiz.quiz });
     }
   }
   onSubmit() {
     alert("submitted");
   }
-  onChange(index){
-    this.setState({activeIndex:index});
+  onChange(index) {
+    this.setState({ activeIndex: index });
   }
-  onNext(){
+  onNext() {
     let index = this.state.activeIndex;
-    if(this.state.quiz.questionsFull && index+1!==this.state.quiz.questionsFull.length){
-        this.setState({activeIndex:index+1});   
+    if (this.state.quiz.questionsFull && index + 1 !== this.state.quiz.questionsFull.length) {
+      this.setState({ activeIndex: index + 1 });
     }
   }
-  onPrev(){
-    let index = this.state.activeIndex; 
-    if(index!==0){
-        this.setState({activeIndex:index-1});
+  onPrev() {
+    let index = this.state.activeIndex;
+    if (index !== 0) {
+      this.setState({ activeIndex: index - 1 });
     }
   }
-  
+  onClearResponse() {
+    let arr = [...this.state.anss];
+    arr[this.state.activeIndex] = -1;
+    this.setState({ anss: arr });
+  }
+  onAnsChange(index, ans) {
+    let arr = [...this.state.anss];
+    arr[index] = ans;
+    this.setState({ anss: arr });
+  }
   render() {
     const { user } = this.props.auth;
     const { name, questionsFull, duration } = this.state.quiz;
@@ -75,16 +130,14 @@ class TakeQuiz extends Component {
           <div className="float-right box-padding-20">
             <div className="user-name float-left mr-3">ID: {user.Id}</div>
             <div className="timer private-timer">
-              <div className="time-widget align-center">
-                Timer - {duration}:00
-              </div>
+              <Timmer Time={duration} onSubmit = {this.onSubmit}/>
             </div>
-            <btn
+            <button
               className="end-test btn btn-danger"
               style={{ color: "white" }}
               onClick={this.onSubmit}>
               End Test
-            </btn>
+            </button>
           </div>
         </div>
         <div className="body">
@@ -98,13 +151,13 @@ class TakeQuiz extends Component {
               </div>
               <div class="left-pane-content">
                 <div class="mt-3">
-                    {questionsFull?questionsFull.map((item,index)=>sideQuestion({index,onChange:this.onChange,question:item.question,qid:item._id,aid:this.state.quiz.questionsFull[this.state.activeIndex]._id})):<br></br>}
+                  {questionsFull ? questionsFull.map((item, index) => sideQuestion({ index, onChange: this.onChange, question: item.question, qid: item._id, aid: this.state.quiz.questionsFull[this.state.activeIndex]._id })) : <br></br>}
                 </div>
               </div>
             </div>
             <div className="right-pane">
               <div className="container">
-                <Question activequestion={this.state.quiz.questionsFull?this.state.quiz.questionsFull[this.state.activeIndex]:{}}/>
+                <Question Index={this.state.activeIndex} onAnsChange={this.onAnsChange} activequestion={this.state.quiz.questionsFull ? this.state.quiz.questionsFull[this.state.activeIndex] : {}} ans={this.state.anss[this.state.activeIndex]} />
               </div>
               <div className="bottom-line ml-5 mb-4">
                 <button className="btn btn-outline-primary btn-round btn-lg" onClick={this.onPrev}>
@@ -113,7 +166,7 @@ class TakeQuiz extends Component {
                 <button className="btn btn-primary btn-lg btn-round ml-3" onClick={this.onNext}>
                   Next
                 </button>
-                <button className="btn btn-outline-secondary btn-round btn-lg ml-5">
+                <button className="btn btn-outline-secondary btn-round btn-lg ml-5" onClick={this.onClearResponse}>
                   Clear Response
                 </button>
                 <button className="btn btn-outline-danger btn-lg btn-round btn-right" onClick={this.onSubmit}>
