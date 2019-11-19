c45df8052c331853f476fbe6f297da6c7554a14a
Yatin Patel <yatinpatel.gt@gmail.com>
2019-11-19 21:35:06 +0530

    admin-home

---
+++ client/src/components/admin-dashboard/Home/LineChart.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import { Line, Pie } from "react-chartjs-2";
+
+const data = {
+  labels: [],
+  datasets: [
+    {
+      label: "Avg. percentage",
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: "rgba(224, 231, 235,0.3)",
+      borderColor: "rgb(131, 160, 153)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgb(57,70,78)",
+      pointBackgroundColor: "rgb(57,70,78)",
+      pointBorderWidth: 5,
+      pointHoverRadius: 7,
+      pointHoverBackgroundColor: "rgb(57, 70, 78)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: []
+    },
+    {
+      label: "Required",
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: "rgba(204, 204, 173,0.4)",
+      borderColor: "rgb(105, 104, 61)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgb(49, 49, 22)",
+      pointBackgroundColor: "rgb(57,70,78)",
+      pointBorderWidth: 5,
+      pointHoverRadius: 7,
+      pointHoverBackgroundColor: "rgb(57, 70, 78)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: []
+    }
+  ]
+};
+
+const  options =  {
+  responsive: true,
+  title: {
+    display: true,
+    text: 'Quiz performace'
+  },
+  scales: {
+    xAxes: [ {
+      display: true,
+      scaleLabel: {
+        display: true,
+        labelString: 'Quiz Name'
+      },
+      ticks:{
+        padding:10
+      }
+    } ],
+    yAxes: [ {
+      display: true,
+      scaleLabel: {
+        display: true,
+        labelString: 'Avg. Percentage'
+      },
+      ticks: {
+        beginAtZero: true,
+        steps: 10,
+        stepValue: 10,
+        max: 100,
+        padding:10
+    }
+    } ]
+  }
+};
+
+class LineChart extends Component {
+  constructor(props){
+    super(props);
+    this.changeData = this.changeData.bind(this);
+    this.state = {
+      ...this.changeData(props.quizs)
+    };
+  }
+  changeData(arr){
+    let arr1 = [];
+    let arr2 = [];
+    let arr3 = [];
+    if(arr && arr.length!==0){
+      arr.forEach((item) => {
+        arr1.push(item.name);
+        arr2.push((Number(item.totalPer) / Number(item.total)).toFixed(2));
+        arr3.push(item.perToPass);
+      });
+    }
+    return {
+      labels:arr1,
+      data:arr2,
+      perToPass:arr3,
+    };
+  }
+  componentWillReceiveProps(nextProps){
+    if(nextProps.quizs){
+      let obj = this.changeData(nextProps.quizs);
+      this.setState({...obj});
+    }
+  }
+  render() {
+    data.labels = this.state.labels;
+    data.datasets[0].data = this.state.data;
+    data.datasets[1].data = this.state.perToPass;
+    return (
+      <div>
+        <Line data={data} options={options} height={250} />
+      </div>
+    );
+  }
+}
+
+export default LineChart;
--- client/src/components/admin-dashboard/Home/home.js
+++ client/src/components/admin-dashboard/Home/home.js
@@ -1,11 +1,100 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchQuizResults } from "../../../actions/fetchActions";
+import Loading from "../../layout/Loading";
+import LineChart from "./LineChart";
 
-export default class Home extends Component {
-    render() {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quizs: [],
+      currentIndex: 0,
+      isLoading: true
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  componentDidMount() {
+    this.props.fetchQuizResults();
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.quizResults) {
+      this.setState({ quizs: nextProps.quizResults });
+      this.setState({ isLoading: false });
+    }
+  }
+  onChange(e) {
+    console.log(e.target.value);
+    this.setState({ currentIndex: e.target.value });
+  }
+  render() {
     return (
-      <div>
-        
+      <div className="container">
+        {this.state.isLoading ? (
+          <Loading />
+        ) : (
+          <div>
+            <div className="row mt-5 ml-4">
+              <div className="col">
+                <LineChart quizs={this.state.quizs} />
+              </div>
+              <div className="col ml-5 mt-4">
+                <div
+                  classNamex="card border-primary"
+                  style={{ maxWidth: "18rem" }}>
+                  <div className="card-body text-prim1ary">
+                    <h5 className="card-title">Total Users: 4</h5>
+                    <p className="card-text">
+                      Admins: 2
+                      <br />
+                      Users: 2
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="card mt-3 border-primary"
+                  style={{ maxWidth: "18rem" }}>
+                  <div className="card-body text-prim1ary">
+                    <h5 className="card-title">Total Quizzes: 10</h5>
+                    <p className="card-text">
+                      Archived: 2
+                      <br />
+                      Ongoing: 2
+                      <br />
+                      Upcoming: 6
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="card mt-3 border-primary"
+                  style={{ maxWidth: "18rem" }}>
+                  <div className="card-body text-prim1ary">
+                    <h5 className="card-title">Total Questions: 20</h5>
+                    <p className="card-text">
+                      Easy: 10
+                      <br />
+                      Medium : 5
+                      <br />
+                      Hard : 5
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  fetchQuizResults: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  quizResults: state.data.quizResults
+});
+
+export default connect(mapStateToProps, { fetchQuizResults })(Home);
